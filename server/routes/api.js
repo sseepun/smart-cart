c6d9a72989fb7739cf917063a7b8764953f1c9f6
Sarun Seepun <ss@acertas-analytics.com>
2017-11-20 15:11:14 -0800

    Setup MEAN app

---
+++ server/routes/api.js
@@ -0,0 +1,59 @@
+const express = require('express');
+const router = express.Router();
+const MongoClient = require('mongodb').MongoClient;
+const ObjectID = require('mongodb').ObjectID;
+
+// Connect
+const connection = (closure) => {
+    return MongoClient.connect('mongodb://localhost:19000/mean', (err, db) => {
+        if (err) return console.log(err);
+
+        closure(db);
+    });
+};
+
+// Error handling
+const sendError = (err, res) => {
+    response.status = 501;
+    response.message = typeof err == 'object' ? err.message : err;
+    res.status(501).json(response);
+};
+
+// Response handling
+let response = {
+    status: 200,
+    data: [],
+    message: null
+};
+
+// Get database collections
+router.get('/users', (req, res) => {
+    connection((db) => {
+        db.collection('users')
+            .find()
+            .toArray()
+            .then((users) => {
+                response.data = users;
+                res.json(response);
+            })
+            .catch((err) => {
+                sendError(err, res);
+            });
+    });
+});
+router.get('/posts', (req, res) => {
+    connection((db) => {
+        db.collection('userPosts')
+            .find()
+            .toArray()
+            .then((users) => {
+                response.data = users;
+                res.json(response);
+            })
+            .catch((err) => {
+                sendError(err, res);
+            });
+    });
+});
+
+module.exports = router;
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,26 @@
 import { Component } from '@angular/core';
 
+// Import the DataService
+import { DataService } from './data.service';
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
+
 export class AppComponent {
-  title = 'app';
+
+  private users: Array<any>;
+  private posts: Array<any>;
+  
+  // Create an instance of the DataService through dependency injection
+  constructor(private dataService: DataService) {
+    // Access the Data Service's getUsers() method we defined
+    this.dataService.getUsers()
+        .subscribe(res => this.users = res);
+    this.dataService.getPosts()
+        .subscribe(res => this.posts = res);
+  }
+
 }
